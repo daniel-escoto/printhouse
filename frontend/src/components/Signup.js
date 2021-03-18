8779f905261f1e470c7cbe8ea26eec8f8adf88a7
Daniel <escotodanny62@gmail.com>
2021-03-17 18:21:58 -0700

    changes

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,10 +1,30 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Form, Button, Card } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+
+    try {
+        setError('')
+        await signup(emailRef.current.value, passwordRef.current.value)
+    } catch {
+        setError('Failed to create an account')
+    }
+
+    signup(emailRef.current.value, passwordRef.current.value);
+  }
 
   return (
     <>
